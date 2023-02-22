6869caf335757fc21eeaaaab85b03ac8b5cfd88a
remotemdp <mdp@mdp>
2023-02-22 20:04:17 +0700

    first commit

---
+++ belajarphp1.php
@@ -0,0 +1,15 @@
+<html>
+
+    <head>
+        <meta charset="UTF-08">
+        <title>Belajar PHP Pert 1</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <?php
+            echo"Hello World"
+        ?>
+
+        
+    </body>
+</html>
---
+++ belajarphp2.php
@@ -0,0 +1,69 @@
+<?php
+//Create Variable
+$nama_asli = "Adrian Chen";
+$nama_samaran = "Luminoir";
+
+
+echo $nama_asli ." ". $nama_samaran;
+//to add new line or paragraph
+echo "<br/>";
+
+
+//to add space use this
+// another way to output a string
+echo "$nama_asli $nama_samaran $";
+
+
+//to add space without using chaining
+echo "<r/  or  n/>";
+
+
+//dont need to declare the type of the variable example
+$umur = 18;
+$string = "ini";
+$double = 0.4;
+$boolean_true = true;
+$boolean_false = false;
+$novalue_or_null = null;
+    //there are two way to add a array type variable
+$hobby = ["Gaming", "Coffe", "Being sad", "Taking a Rest", "Speeding"];
+$hobby2 = array("Gaming, Sleep, Jerk Off");
+//so you dont need to literally type the datatype
+
+echo "<br/>";
+// check the what data type is inside the variable use var_dump() e.g:
+var_dump($umur);
+echo "<br/>";
+var_dump($string);
+echo "<br/>";
+var_dump($double);
+echo "<br/>";
+var_dump($boolean_false);
+echo "<br/>";
+var_dump($boolean_true);
+echo "<br/>";
+var_dump($novalue_or_null);
+echo "<br/>";
+var_dump($hobby);
+echo "<br/>";
+var_dump($hobby2);
+echo "<br/>";
+
+
+//list of array function
+    // to count total of how many item in the array
+    echo count($hobby);
+    echo "<br/>";
+    echo count($hobby2) ;
+    echo "<br/>";
+
+
+    // to merge 2 array or more;
+    $hobby3 = array_merge($hobby, $hobby2);
+    echo "<br/>";
+    
+    //to show the item inside of the array use print_r e.g:
+    print_r($hobby);
+    print_r($hobby2);
+    print_r($hobby3);
+?>
